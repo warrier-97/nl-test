f38decce00136ca509a14f70ed01c3bd312cb7a0
Aditya Warrier <aditya.warrier.unni2@gmail.com>
2020-09-24 14:50:20 +0530

    Added a loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React, { useEffect, useState, memo } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Repository } from 'domain/repositories/Repository';
 import { CardContainer } from 'components/CardContainer/CardContainer';
 import { Item } from 'domain/models/Item';
 
 const App = (): React.ReactElement => {
   const [stories, setStories] = useState<Item[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getStories = async (): Promise<void> => {
-      const stories = await Repository.getStories();
-      setStories(stories);
+      try {
+        const stories = await Repository.getStories();
+        setStories(stories);
+      } catch (err) {
+        console.warn('API has failed!');
+      }
+      setLoading(false);
     };
 
     getStories().then();
-  }, []);
+  }, [stories]);
 
-  return <CardContainer stories={stories} />;
+  return !loading ? <CardContainer stories={stories} /> : <Spinner animation="border" />;
 };
 
 const memoizedComponent = memo(App);
